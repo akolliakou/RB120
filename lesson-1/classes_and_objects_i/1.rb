7e5d09943ed8d76d2a4d18e1553068dea8a0be17
Anna Kolliakou <akolliakou@gmail.com>
2020-09-30 19:27:28 +0100

    Add first lesson-1 exercises

---
+++ lesson-1/classes_and_objects_i/1.rb
@@ -0,0 +1,35 @@
+class MyCar
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @speed = 0
+  end
+
+  def speed_up(number)
+    @speed += number
+    puts "Step on the gas and increase speed by #{number} kmph"
+  end
+
+  def brake(number)
+    @speed -= number
+    puts "Step on the brake and decrease speed by #{number} kmph"
+  end
+
+  def current_speed
+    puts "You are going at #{@speed} kmph"
+  end
+  
+  def shut_down
+    @speed = 0
+    puts "You are now stopped"
+  end
+end
+
+toyota = MyCar.new(2005, 'silver', 'corolla')
+toyota.speed_up(10)
+toyota.current_speed
+toyota.brake(10)
+toyota.current_speed
+toyota.shut_down
+toyota.current_speed
