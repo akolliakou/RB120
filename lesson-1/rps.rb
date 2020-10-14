eb1351070d41365ddbdcb1b3df690cc38da050e5
Anna Kolliakou <akolliakou@gmail.com>
2020-10-14 10:19:01 +0100

    More RPS

--- lesson-1/rps.rb
+++ lesson-1/rps.rb
@@ -18,3 +18,56 @@ end
 
 def compare
 end
+
+class Player
+  attr_reader :move
+
+  def initialize(player_type)
+    @player_type = player_type
+    @move = nil
+  end
+
+  def choose
+    if human?
+      choice = nil
+      loop do
+        puts "Please choose rock, paper or scissors:"
+        choice = gets.chomp
+        break if ['rock', 'paper', 'scissors'].include?(choice)
+        puts "Sorry, invalid choice."
+      end
+      self.move = choice
+    else
+      self.move = ['rock', 'paper', 'scissors'].sample
+    end
+  end
+
+  def human?
+    @player_type == :human
+  end
+end
+
+class RPSGame
+  def initialize
+    @human = Player.new(:human)
+    @computer = Player.new(:player)
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors. Goodbye!"
+  end
+
+  def play
+    display_welcome_message
+    human_choose_move
+    computer_choose_move
+    display_winner
+    display_goodbye_message
+  end
+end
+
+RPSGame.new.play
